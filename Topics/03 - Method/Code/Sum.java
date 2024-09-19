403d065d1770f0ea264f5c2c1eb15786bc2c9c07
Shivendra singh <shivendra7083@gmail.com>
2024-09-20 01:25:41 +0530

    intro of method fn

--- Topics/03 - Method/Code/Sum.java
+++ Topics/03 - Method/Code/Sum.java
@@ -1,5 +1,21 @@
-package Topics.03 - Method.Code;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class Sum {
-    
+    public static void main(String[] args) {
+        sum();  // Calling functon / method with brackets
+        sum();  // we call method multiple times
+        System.out.println("Print using method");
+    }
+
+    static void sum () {    // Inside bracket Parameter list
+        Scanner sc  = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter number 2: ");
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+    }
+
 }
