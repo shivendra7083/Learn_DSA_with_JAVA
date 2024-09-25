36794ff5d185e758c9dfa7df79697ea0563f35a3
Shivendra singh <shivendra7083@gmail.com>
2024-09-26 01:40:09 +0530

    Find the Largest and Smallest of Three Numbers

---
+++ Questions/Method/Lar_Smal_3_Digit.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Lar_Smal_3_Digit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number 1 :");
+        int a = sc.nextInt();
+        System.out.print("ENter number 2 :");
+        int b = sc.nextInt();
+        System.out.print("Enter number 3 :");
+        int c = sc.nextInt();
+        int largest = largest(a , b , c) ;
+        int smallest = smallest(a ,b ,c) ;
+        System.out.println("The largest number is : " + largest );
+        System.out.print("The smallest number is : "+ smallest);
+
+    }
+    public static int largest(int a ,int b , int c) {
+        int max = a ;
+        if (max < b){
+            max = b ;
+        }
+        if(max < c) {
+            max = c ;
+        }
+        return max ;
+    }
+    public static int smallest(int a,int b , int c ) {
+        int min = a ;
+        if (min>b){
+            min = b ;
+        }
+        if (min > c) {
+            min = c ;
+        }
+        return min ;
+    }
+}
