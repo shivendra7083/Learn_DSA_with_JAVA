9fcf6baa369f3da79562daa59718122534f45554
Shivendra singh <shivendra7083@gmail.com>
2024-09-25 23:52:57 +0530

    very basic Electric bill prog...

---
+++ Questions/Conditon and loops/ElectricBill.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ElectricBill {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter unit : ");
+        int unit = sc.nextInt();
+        System.out.println("The gernarted bill is : " + Calculatebill(unit));
+    }
+    public static double Calculatebill(int unit) {
+        double bill = 0 ;
+        double unitPrice = 2.50 ;
+        bill = unit * unitPrice;
+        double tax = 5.25 ;
+        bill += tax ;
+        return bill ;
+
+    }
+}
