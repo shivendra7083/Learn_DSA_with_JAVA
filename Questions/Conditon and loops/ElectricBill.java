ef05a77620d08ee61bf62f6e6562c7cf5545fd1d
Shivendra singh <shivendra7083@gmail.com>
2024-09-26 00:00:03 +0530

    very basic Electric bill prog....

--- Questions/Conditon and loops/ElectricBill.java
+++ Questions/Conditon and loops/ElectricBill.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class ElectricBill {
+    // Basic electric bill
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter unit : ");
