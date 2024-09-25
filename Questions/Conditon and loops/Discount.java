faa6ffbb842557ff7424a007e5b5a427d684cb50
Shivendra singh <shivendra7083@gmail.com>
2024-09-26 00:54:03 +0530

    Calculate discount of product

---
+++ Questions/Conditon and loops/Discount.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+// Q. Calculate Discount Of Product
+public class Discount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        System.out.print("Enter the original amount of product : ");
+        float OriginalAmount = sc.nextFloat();
+        System.out.print("Discount percent : ");
+        float Dpercent = sc.nextFloat();
+        double discount = OriginalAmount * (Dpercent/100) ;
+        System.out.print("FInal price of product is : ");
+        double Finalprice = (OriginalAmount - discount) ;
+        System.out.println(Finalprice);
+    }
+}
