8eb6cbba323a37e122b478fbd1ffa1ea78e53b88
Shivendra singh <shivendra7083@gmail.com>
2024-09-28 23:49:05 +0530

    Input in array

--- Topics/04-Arrays/codes/src/Main.java
+++ Topics/04-Arrays/codes/src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
+
     public static void main(String[] args) {
-        // Q. Store a roll no
+        // Q: store a roll number
+        int a = 19;
+
+        // Q: store a person's name
+        String name = "Shivendra singh";
+
+        // Q: store 5 roll numbers
+        int rno1 = 23;
+        int rno2 = 55;
+        int rno3 = 18;
+            // Syntax of arrray
+        // datatype[] variable name = new datatype [size] ;
+
+        // Store roll no
+//        int [] rnos = new int [5] ;
+//        // or directly
+//        int [] rnos2 = {15,85,68,45,62,} ;
+
+        int[] ros; // declaration of array. ros is getting defined in the stack
+        ros = new int[5]; // initialisation: actually here object is being created in the memory (heap)
+//        System.out.println(ros[0]);
+
     }
 }
