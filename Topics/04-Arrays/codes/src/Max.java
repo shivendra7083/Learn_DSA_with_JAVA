e0c1221b904abcd18467b472846614c1326c10a6
Shivendra singh <shivendra7083@gmail.com>
2024-10-08 23:18:47 +0530

    Fix typo in comment for max method in Max.java
    Adjusted the spacing in the comment to ensure clarity. This improves the readability of the code by correcting the minor formatting issue. No functional changes were made.

--- Topics/04-Arrays/codes/src/Max.java
+++ Topics/04-Arrays/codes/src/Max.java
@@ -3,7 +3,7 @@ public class Max {
         int [] arr = {1,2,3,4,5,10,5,40,100,5,30,1024, 10};
         System.out.println(max(arr));
     }
-    // Imagine the arr  is not empty
+    // Imagine the arr   is not empty
     static int max(int [] arr) {
         int max = arr [0] ;
         for (int i = 1; i < arr.length; i++) {
