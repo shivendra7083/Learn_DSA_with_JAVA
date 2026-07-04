e850e98ac3b368a0fbab7f3fd47462c39c08d527
Shivendra singh <shivendra7083@gmail.com>
2026-07-04 22:35:17 +0530

    feat: update main method to use pattern2 and modify pattern1 for better clarity

--- Topics/09-Pattern/code/Pattern.java
+++ Topics/09-Pattern/code/Pattern.java
@@ -1,12 +1,21 @@
 public class Pattern {
     public static void main(String[] args) {
-        int n = 5;
-        pattern1(n);
+        int n = 6;
+//        pattern1(n);
+        pattern2(n);
     }
 
     static void pattern1(int n) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j < n; j++) {
+        for (int rows = 1; rows <= n; rows++) {
+            for (int col = 1; col < n; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern2 (int n) {
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= i ; j++) {
                 System.out.print("*");
             }
             System.out.println();
