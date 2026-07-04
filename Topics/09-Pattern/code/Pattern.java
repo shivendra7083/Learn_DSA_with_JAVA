0d69a59c28f4ff004aca0c110388e6c5b54fb1d7
Shivendra singh <shivendra7083@gmail.com>
2026-07-04 22:19:35 +0530

    feat: add pattern1 method to generate asterisks pattern

--- Topics/09-Pattern/code/Pattern.java
+++ Topics/09-Pattern/code/Pattern.java
@@ -1,7 +1,16 @@
-public  class Pattern {
+public class Pattern {
     public static void main(String[] args) {
-
+        int n = 5;
+        pattern1(n);
     }
 
+    static void pattern1(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
 
