cad7ccfd9743cb6dfeb321676b086e6e05fcb212
Shivendra singh <shivendra7083@gmail.com>
2026-07-13 22:20:28 +0530

    refactor: improve formatting and consistency in pattern methods

--- Topics/09-Pattern/code/Pattern.java
+++ Topics/09-Pattern/code/Pattern.java
@@ -18,26 +18,26 @@ public class Pattern {
         }
     }
 
-    static void pattern2 (int n) {
-        for (int i = 1; i <= n ; i++) {
-            for (int j = 1; j <= i ; j++) {
+    static void pattern2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 
-    static void pattern3 (int n) {
-        for (int i = 1; i <= n ; i++) {
-            for (int j = 1; j <= n-i+1; j++) {
+    static void pattern3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 
-    static void pattern4 (int n) {
-        for (int i = 1; i <= n ; i++) {
+    static void pattern4(int n) {
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j);
             }
@@ -46,12 +46,12 @@ public class Pattern {
     }
 
     static void pattern5(int n) {
-        for(int i = 1 ; i <= 2*n - 1 ; i++) {
-            int stars = i ;
-            if(i >n) {
-                stars = 2*n- i ;
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            int stars = i;
+            if (i > n) {
+                stars = 2 * n - i;
             }
-            for(int j = 1 ; j<= stars ; j++ ) {
+            for (int j = 1; j <= stars; j++) {
                 System.out.print("*");
             }
             System.out.println();
